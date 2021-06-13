c9f0bd4dd657a141b803465db3b5fad6d3c3ced1
Algo-k <61967547+Algo-k@users.noreply.github.com>
2021-06-13 21:21:46 +0430

    Add files via upload

---
+++ UML/Code.cs
@@ -0,0 +1,52 @@
+﻿
+
+
+using System.Windows.Forms;
+using Ctrl;
+using Ui;
+
+
+class UiExample : UiCreator
+{
+    public override void Begin()
+    {
+    }
+    public override void Body()
+    {
+    }
+    public override void End()
+    {
+    }
+
+    public override void Create(params object[] values)
+    {
+    }
+
+}
+
+class AppContext : ApplicationContext
+{
+    static public Loop Loop = new Loop();
+
+    public AppContext()
+    {
+        ParentWin parent = ParentWin.Instance();
+        parent.Show();
+        parent.FormClosed += new FormClosedEventHandler(parent_FormClosed);
+
+
+        var win = SingelWin.Instance(new UiExample(), "Explorer");
+        win.Resize((Screen.PrimaryScreen.Bounds.Width * 3) / 4, (Screen.PrimaryScreen.Bounds.Height * 3) / 4);
+        win.Show(false);
+
+        var pro = SingelWin.Instance(new UiExample(), "Properties");
+        pro.Resize(100, 100);
+        pro.Show(false);
+        pro.ToolDock(DockStyle.Right);
+    }
+
+    void parent_FormClosed(object sender, FormClosedEventArgs e)
+    {
+        ExitThread();
+    }
+}
